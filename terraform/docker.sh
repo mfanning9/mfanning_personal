e76ec9571f38cb81e1af887e39257506ead32ce0
mfanning <mfanning@users.noreply.github.com>
2025-07-09 16:02:24 -0400

    Checking in install notes / initial main.tf commit following instructions

--- terraform/docker.sh
+++ terraform/docker.sh
@@ -5,3 +5,9 @@ docker run \
   -p 127.0.0.1:4510-4559:4510-4559 \
   -v /var/run/docker.sock:/var/run/docker.sock \
   localstack/localstack
+
+  # Docker Desktop > Settings > Resources > Enable integration with other distros, had to do that after moving to ubuntu 22
+
+  # Local stack start via their CLI and no longer above
+  
+   # localstack config validate
