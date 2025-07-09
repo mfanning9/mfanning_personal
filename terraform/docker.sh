b07659ef19d6bc50120b54820847acc095706ccb
Matthew Fanning <mfanning@users.noreply.github.com>
2025-07-09 14:22:49 -0400

    Checking in changes before re-build WSL ubuntu on 22

---
+++ terraform/docker.sh
@@ -0,0 +1,7 @@
+# should also look at installing miniKube and deploying via helm https://docs.localstack.cloud/aws/getting-started/installation/#docker
+docker run \
+  --rm -it \
+  -p 127.0.0.1:4566:4566 \
+  -p 127.0.0.1:4510-4559:4510-4559 \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  localstack/localstack
